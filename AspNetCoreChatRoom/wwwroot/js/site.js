263191933dcf0fa89b94df31d7d9ae1d7053b887
MHefny <mhefnydev@gmail.com>
2020-02-12 23:45:36 +0200

    Initial commit

---
+++ AspNetCoreChatRoom/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Write your Javascript code.
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+
+
+  $(function(){
+    $("#addClass").click(function () {
+              $('#qnimate').addClass('popup-box-on');
+                });
+              
+                $("#removeClass").click(function () {
+              $('#qnimate').removeClass('popup-box-on');
+                });
+      })
